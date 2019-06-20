13f25df012f96c34e14e91be49aab9589deb7f92
navnelson7 <navnelson7@gmail.com>
2019-06-20 12:55:13 -0600

    Conteo en tiempo real primer equipo funcionado

--- conteo.php
+++ conteo.php
@@ -3,11 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Conteo</title>
-    <!--<link rel="stylesheet" href="contstilo.css"> --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="contstilo.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   </head>
   <body>
-   <div class="container">
+  <div id="app">
+  <div class="container">
    <div class="logos">
       <center><h3>TERCER CERTAMEN DE DEBATES INSTITUCIONAL</h3></center>
       <center><img src="debatelog.png" alt="" widht="300px" height="300px"></center>
@@ -17,7 +19,7 @@
     ?>
     <div class="puntos">
       <div class="tabla-1">
-        <form class="text" action="ganador.php" method="post">
+        <form class="text" action="" method="post">
       <div class="row">
         <div class="col">
         <?php
@@ -30,7 +32,7 @@
             1.1 Orden logico de la exposicion 
             </div>
             <div class="col">
-            <select class="" name="orden_logico_exposicion">
+            <select class="" name="1.1equipo1" v-model="item1equipo1" v-on:click="sumar">       
               <option value="0">0</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -47,7 +49,7 @@
                 1.2 Coherencia 
               </div>
               <div class="col">
-                  <select class="" name="coherencia">
+                  <select class="" name="1.2equipo1"  v-model="item2equipo1" v-on:click="sumar">
                   <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -64,7 +66,7 @@
                   1.3 exposicion clara
                 </div>
                 <div class="col">
-                    <select class="" name="coordinacion_miembros">
+                    <select class="" name="1.3equipo1" v-model="item3equipo1" v-on:click="sumar">
                     <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -82,7 +84,7 @@
               1.4 exposicion coordinada entre miembros
               </div>
               <div class="col">
-                <select class="" name="coordinacion_miembros">
+                <select class="" name="1.4equipo1" v-model="item4equipo1" v-on:click="sumar">
                   <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -104,7 +106,7 @@
                 2.1 oratoria 
               </div>
               <div class="col">
-              <select class="" name="oratoria">
+              <select class="" name="2.1equipo1" v-model="item5equipo1" v-on:click="sumar">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -121,7 +123,7 @@
                 2.2 Diccion 
               </div>
               <div class="col">
-                <select class="" name="diccion">
+                <select class="" name="2.2equipo1" v-model="item6equipo1" v-on:click="sumar">
                   <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -138,7 +140,7 @@
                 2.3 Expresion corporal 
               </div>
               <div class="col">
-                <select class="" name="expresion_corporal">
+                <select class="" name="2.3equipo1" v-model="item7equipo1" v-on:click="sumar">
                   <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -155,7 +157,7 @@
                 2.4 Capacidad de captar atencion 
               </div>
               <div class="col">
-                <select class="" name="capatar_atencion">
+                <select class="" name="2.4equipo1" v-model="item8equipo1" v-on:click="sumar">
                   <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -177,7 +179,7 @@
                   3.1 Capacidad de sostener un argumento 
                 </div>
                 <div class="col">
-                  <select class="" name="sostener_argumento">
+                  <select class="" name="3.1equipo1" v-model="item9equipo1" v-on:click="sumar">
                     <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -194,7 +196,7 @@
                 3.2 Capacidad de refutar argumentos contrarios 
               </div>
               <div class="col">
-              <select class="" name="refutar_argumentos">
+              <select class="" name="3.2equipo1" v-model="item10equipo1" v-on:click="sumar">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -211,7 +213,7 @@
                 3.3 Tecnica de debate 
               </div>
               <div class="col">
-              <select class="" name="tecnica">
+              <select class="" name="3.3equipo1" v-model="item11equipo1" v-on:click="sumar">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -234,7 +236,7 @@
                 4.1 Uso efectivo de citas e investigacion 
               </div>
               <div class="col">
-              <select class="" name="investigacion">
+              <select class="" name="4.1equipo1" v-model="item12equipo1" v-on:click="sumar">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -252,7 +254,7 @@
               4.2 Diversidad de fuentes 
               </div>
               <div class="col">
-              <select class="" name="diversidad">
+              <select class="" name="4.2equipo1" v-model="item13equipo1" v-on:click="sumar">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -270,7 +272,7 @@
                   4.3 Relevancia 
               </div>
               <div class="col">
-              <select class="" name="relevancia">
+              <select class="" name="4.3equipo1" v-model="item14equipo1" v-on:click="sumar">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -284,9 +286,9 @@
           
           
           
-          <input type="submit" name="" value="Calificar">
+          <button v-on:click="sumar">Calificar</button>
         </form>
-        <input type="text">
+        <input type="text" v-model="contadorequipo1" ><!-- counterone here -->
       </div>
         <div class="col"><!-- segunda tabla alineada a la derecha -->
         <div class="row">
@@ -557,7 +559,7 @@
       </div>
       <input type="submit" name="" value="Calificar">
         </form>
-        <input type="text">
+        <input type="text" v-model="contadorequipo2"><!-- countertwo here -->
     <?php
     //echo $_REQUEST['hora'];
     ?>
@@ -572,7 +574,9 @@
     mysqli_close($conexion);
     ?>
    </div>
+  </div>
     <!-- development version, includes helpful console warnings -->
   <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <script src="js/conteo.js"></script>
   </body>
 </html>
